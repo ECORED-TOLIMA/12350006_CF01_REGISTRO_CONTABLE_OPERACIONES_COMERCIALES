1d164ac5fdb5fb0187d02c8241d3743f4615c3be
cveimarsena <vcelis@sena.edu.co>
2025-09-24 11:43:12 -0500

    Fuentes, cfa

--- src/config/global.js
+++ src/config/global.js
@@ -203,7 +203,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CFA_XX_DU.pdf',
+        download: 'downloads/CFA_01_12350006_DU.pdf',
       },
       {
         icono: 'fas fa-download',
